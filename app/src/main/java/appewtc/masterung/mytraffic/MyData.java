914efb25bc2f3af29451cbce019e2747cf6cfc73
masterUNG <phrombutr@gmail.com>
2017-05-20 15:04:37 +0700

    Finish 20May

--- app/src/main/java/appewtc/masterung/mytraffic/MyData.java
+++ app/src/main/java/appewtc/masterung/mytraffic/MyData.java
@@ -5,4 +5,23 @@ package appewtc.masterung.mytraffic;
  */
 
 public class MyData {
+
+    //Explicit
+    private String[] titleStrings = new String[]{"หัวข้อที่ 1", "หัวข้อที่ 2", "หัวข้อที่ 3",
+            "หัวข้อที่ 4", "หัวข้อที่ 5", "หัวข้อที่ 6", "หัวข้อที่ 7", "หัวข้อที่ 8", "หัวข้อที่ 9",
+            "หัวข้อที่ 10", "หัวข้อที่ 11", "หัวข้อที่ 12", "หัวข้อที่ 13", "หัวข้อที่ 14", "หัวข้อที่ 15",
+            "หัวข้อที่ 16", "หัวข้อที่ 17", "หัวข้อที่ 18", "หัวข้อที่ 19", "หัวข้อที่ 20"};
+
+    private String[] detailStrings;
+
+    public String[] getTitleStrings() {
+        return titleStrings;
+    }
+
+    public String[] getDetailStrings() {
+
+        String[] resutlStrings;
+
+        return detailStrings;
+    }
 }   // Main Class นี่คือ คลาสหลัก
